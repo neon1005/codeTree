7d77163193c7df37e481b40856b8f90db22f6343
neon1005 <161691786+neon1005@users.noreply.github.com>
2026-08-06 10:37:06 +0900

    [Passed] 따옴표 출력 (2ms, 0MB)

---
+++ trail0/따옴표 출력/print-quote.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string sentence = "He says \"It's a really simple sentence\".";
+    cout << sentence <<endl;
+    return 0;
+}
