56f51a6e991fd1625611fc621dea3ecd2d8ee355
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-07 16:27:17 +0900

    [Passed] 정상적인 구구단 (2ms, 0MB)

---
+++ trail0/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+
+    for (int i = 1; i<=n;i++) {
+        for (int j = 1; j<=n;j++) {
+            if (j == n) cout<<i<<" * "<<j<<" = "<<i*j;
+
+            else cout<<i<<" * "<<j<<" = "<<i*j<<", ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
