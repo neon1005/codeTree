f34ac81cd58d6fed239235d3c0c5078a5360b5dd
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-10 15:13:07 +0900

    [Passed] 다양하게 출력 (2ms, 0MB)

---
+++ trail1/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double d = 3.1415926535;
+    cout<<"Total days in Year"<<endl<<365<<endl<<"Circumference rate"<<endl;
+    cout<<fixed;
+    cout.precision(10);
+    cout<<d;
+    return 0;
+}
