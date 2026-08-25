5a76b8cee123f94d3fcb1caa7ee847c815cf2979
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-26 00:32:27 +0900

    [Passed] 달에서 무게 구하기 (1ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int weight = 13;
+    double gMoon = 0.165;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<weight<<" * "<<gMoon<<" = "<<weight*gMoon;
+    return 0;
+}
