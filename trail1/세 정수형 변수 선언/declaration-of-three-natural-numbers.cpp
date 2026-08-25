0149fd4099dbdae84c76c09673ec6499c8e9d9bc
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-26 00:25:56 +0900

    [Passed] 세 정수형 변수 선언 (2ms, 0MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num1 = 7;
+    int num2 = 23;
+    int num3 = 30;
+    cout<<num1<<" + "<<num2<<" = "<<num3;
+    return 0;
+}
