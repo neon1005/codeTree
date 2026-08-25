effb4ce1eb0b2a36096479b0c80a18e394cdc0b4
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-26 00:22:41 +0900

    [Passed] 정수 선언하고 뺄셈 (2ms, 0MB)

---
+++ trail1/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num1 = 97;
+    int num2 = 13;
+    cout<<num1<<" - "<<num2<<" = "<<num1-num2<<endl;
+    return 0;
+}
