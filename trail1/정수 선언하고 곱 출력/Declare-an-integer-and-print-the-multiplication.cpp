6ad91fb66f92b9685a335a4580fed9c159fdfc84
상욱 <161691786+neon1005@users.noreply.github.com>
2026-08-26 00:24:53 +0900

    [Passed] 정수 선언하고 곱 출력 (1ms, 0MB)

---
+++ trail1/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num1 = 26;
+    int num2 = 5;
+    cout<<num1<<" * "<<num2<<" = "<<num1*num2;
+    return 0;
+}
