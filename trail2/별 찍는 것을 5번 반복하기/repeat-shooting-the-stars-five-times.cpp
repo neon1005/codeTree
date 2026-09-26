762d0cd7b8abaf59d996b2ffc91a28fd7aa9abc6
상욱 <161691786+neon1005@users.noreply.github.com>
2026-09-26 21:33:33 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void function() {
+    cout<<"**********"<<endl;
+}
+int main() {
+    // Please write your code here.
+    for (int i = 0; i<5;i++) {
+        function();
+    }
+    return 0;
+}
