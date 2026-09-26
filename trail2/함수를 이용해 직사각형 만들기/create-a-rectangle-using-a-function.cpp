c68b0db04551ae70a0129fd2cb8918426ea287f2
상욱 <161691786+neon1005@users.noreply.github.com>
2026-09-26 21:42:48 +0900

    [Passed] 함수를 이용해 직사각형 만들기 (3ms, 0MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void function(int n, int m) {
+    for (int i = 0; i<n;i++) {
+        for (int j = 0; j<m;j++) {
+            cout<<1;
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    // Please write your code here.
+    int n,m;
+    cin>>n>>m;
+    function(n, m);
+    return 0;
+}
