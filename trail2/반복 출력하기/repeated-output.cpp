283196a418fe29b22c733a20e6c5b789b9cabed0
상욱 <161691786+neon1005@users.noreply.github.com>
2026-09-26 21:40:24 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void function(int n) {
+    for (int i = 0; i<n; i++) {
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    function(n);
+    return 0;
+}
